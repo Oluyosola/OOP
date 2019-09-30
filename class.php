580de29d45926d6f78cfa9d61580851ca735f5ca
0luyosola <afolabioluyosola@gmail.com>
2019-09-30 10:03:04 +0100

    oop

---
+++ class.php
@@ -0,0 +1,21 @@
+<?php
+class person{
+    public $name;
+    public $age;
+    public function sleep(){
+        echo "I'm sleeping";</br>
+    }
+        public function eat(){
+        echo "I want Amala";
+    }
+            public function walk(){
+                echo "I am almost there";
+            }
+        }
+
+
+    $test = new person();
+    $test ->sleep();
+    $test->eat();
+    $test->walk();
+
